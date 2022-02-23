49488c27b21540629c7cb8159f1d9770732931a2
Ira Markova <imarkova.js@gmail.com>
2022-02-23 13:42:07 +0200

    Добавила вызов модалки на кнопку франчизы
    в скрипте добавила 3 вариант

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,6 +2,7 @@
   const refs = {
     openModalBtn1: document.querySelector('[data-modal-open1]'),
     openModalBtn2: document.querySelector('[data-modal-open2]'),
+    openModalBtn3: document.querySelector('[data-modal-open3]'),
 
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
@@ -9,6 +10,7 @@
 
   refs.openModalBtn1.addEventListener('click', toggleModal);
   refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.openModalBtn3.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
